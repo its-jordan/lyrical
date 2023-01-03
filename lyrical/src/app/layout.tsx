8a5e2bc5a7fa52eaf734edfce1faf1d247779123
DoomedSchultze <45129838+DoomedSchultze@users.noreply.github.com>
2023-01-03 12:18:17 -0600

    Updated Styling, Added rudimentary navigation

--- lyrical/src/app/layout.tsx
+++ lyrical/src/app/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({
     <html>
       <head />
       <body>
-        <nav className="fixed z-50 flex h-12 w-full flex-row flex-nowrap items-center justify-center bg-white/50 px-4 backdrop-blur-sm">
+        <nav className="fixed z-50 flex h-12 w-full flex-nowrap items-center justify-center bg-white/50 px-4 backdrop-blur-sm">
           <Link href="songs/">View Song Directory</Link>
         </nav>
         <div>{children}</div>
